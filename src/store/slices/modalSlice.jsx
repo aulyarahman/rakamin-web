dcbe54c9f7dccbd07c7911d07852bed224600c78
aulyarahman <76601895+aulyarahman@users.noreply.github.com>
2022-08-05 02:24:17 +0800

    add setting modal slice to redux

---
+++ src/store/slices/modalSlice.jsx
@@ -0,0 +1,59 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+
+export const initialConfirmationModalState = {
+    isOpened: false,
+    isConfirmed: false,
+    isDeclined: false,
+}
+
+
+export const confirmationModalThunkActions = {
+    open: createAsyncThunk('confirmationModal', async (_, thunkAPI) => {
+    const store = thunkAPI.extra?.store
+    thunkAPI.dispatch(confirmationModalActions.open())
+    return new Promise((resolve, reject) => {
+        const unsubscribe = store.subscribe(() => {
+            const state = thunkAPI.getState()
+
+            if (state?.modal.isConfirmed) {
+                unsubscribe()
+                resolve(true)
+            }
+            if (state?.modal.isDeclined) {
+                unsubscribe()
+                reject(false)
+            }
+        })
+    })
+}),
+}
+
+
+const confirmationModalSlice = createSlice({
+    name: 'confirmationModal',
+    initialState: initialConfirmationModalState,
+    reducers: {
+        open: (state) => {
+            state.isOpened = true
+            state.isDeclined = false
+            state.isConfirmed = false
+        },
+        confirm: (state) => {
+            state.isConfirmed = true
+            state.isOpened = false
+        },
+        decline: (state) => {
+            state.isDeclined = true
+            state.isOpened = false
+        },
+        setContent: (_, action) => {
+            return { content: action.payload.content }
+        },
+    },
+})
+
+const { reducer, actions } = confirmationModalSlice
+export const confirmationModalActions = confirmationModalSlice.actions
+
+export default reducer
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import modalReducer from './slices/modalSlice.jsx'
+
+const thunkArguments = {}
 
 export const store = configureStore({
-  reducer: {},
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      thunk: {
+        extraArgument: thunkArguments,
+      },
+    })
+  },
+  reducer: {
+    modal: modalReducer,
+  },
 });
+
+thunkArguments.store = store
