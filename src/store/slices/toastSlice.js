5e16656ee855d58240b9fde94453e78cf5326095
aulyarahman <76601895+aulyarahman@users.noreply.github.com>
2022-08-05 16:57:18 +0800

    add toast slices

---
+++ src/store/slices/toastSlice.js
@@ -0,0 +1,62 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import useConfirmationToastManagement from '../../hooks/useToastConfirm';
+
+export const initialState = {
+  isOpened: false,
+  isConfirmed: false,
+  isFailed: false,
+  status: 'success' | 'error' | 'pending' | undefined,
+  message: ''
+};
+
+export const toastThunkActions = {
+  open: createAsyncThunk('toast', async (_, thunkAPI) => {
+    const store = thunkAPI.extra?.store;
+    thunkAPI.dispatch(useConfirmationToastManagement.open());
+    return new Promise((resolve, reject) => {
+      const unsubscribe = store.subscribe(() => {
+        const state = thunkAPI.getState();
+        if (state?.modal.isConfirmed) {
+          unsubscribe();
+          resolve(true);
+        }
+        if (state?.modal.isFailed) {
+          unsubscribe();
+          reject(false);
+        }
+      });
+    });
+  })
+};
+
+const confirmationToastSlice = createSlice({
+  name: 'toast',
+  initialState,
+  reducers: {
+    open: (state) => {
+      state.isOpened = true;
+      state.isFailed = false;
+      state.isConfirmed = false;
+      state.status = 'pending';
+    },
+    success: (state) => {
+      state.isConfirmed = true;
+      state.isOpened = false;
+      state.status = 'success';
+    },
+    failed: (state) => {
+      state.isFailed = true;
+      state.isOpened = false;
+      state.status = 'error';
+    },
+    setStatus: (state, action) => {
+      state.status = action.payload.status;
+      state.messages = action.payload.messages;
+    }
+  }
+});
+
+const { reducer, actions } = confirmationToastSlice;
+export const confirmationToastActions = confirmationToastSlice.actions;
+
+export default reducer;
