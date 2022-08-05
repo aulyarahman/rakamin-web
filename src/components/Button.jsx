152eb38596bee5ec43f9be4ec8c004f08d14481d
aulyarahman <76601895+aulyarahman@users.noreply.github.com>
2022-08-05 14:13:50 +0800

    add toast compoentns & rewrite container

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,20 +1,10 @@
-import React from "react";
-import { PropTypes } from "prop-types";
-
-/**
- * @description Hello **world**!
- * @typedef {{
- *  children: React.ReactNode
- *  onClick: React.MouseEventHandler<HTMLButtonElement> | undefined
- * }}
- * Props
- *
- * @type React.FunctionComponent<Props>
- */
+import React from 'react';
+import clsx from 'clsx';
+import { PropTypes } from 'prop-types';
 
 const Button = (props) => {
   const css =
-    "text-white py-[4px] px-[16px] bg-primary h-[28px] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm text-center inline-flex items-center rounded-sm";
+    'text-white py-[4px] px-[16px] bg-primary h-[28px] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm text-center inline-flex items-center rounded-sm';
 
   return (
     <button onClick={props.onClick} className={css}>
@@ -25,11 +15,18 @@ const Button = (props) => {
 
 export default Button;
 
-export const Buttons = (props) => (
+export const Buttons = ({ type = 'button', children }) => (
   <button
-    type="button"
-    className="text-primary text-sm hover:text-white border border-primary focus:ring-4 focus:outline-none font-medium rounded-[5px] text-center px-[8px] py-[2px]"
-  >
-    {props.children}
+    type={type}
+    className={clsx(
+      'text-primary text-sm hover:text-white border border-primary focus:ring-4 focus:outline-none',
+      'font-medium rounded-[5px] text-center px-[8px] py-[2px]'
+    )}>
+    {children}
   </button>
 );
+
+Button.PropTypes = {
+  children: PropTypes.node.isRequired,
+  type: PropTypes.string
+};
