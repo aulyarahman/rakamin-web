5c56c000f785be378953b472c28a68ea8f8cc98b
aulyarahman <76601895+aulyarahman@users.noreply.github.com>
2022-08-07 02:43:50 +0800

    set types colors

--- src/utils/Colors.js
+++ src/utils/Colors.js
@@ -1,3 +1,32 @@
 export const Colors = {
-  primary: "#01959F",
+  primary: '#01959F',
+  secondary: '#FEEABC',
+  success: '#43936C',
+  'success-border': '#B8DBCA',
+  neutral: '#FAFAFA',
+  'danger-border': '#F5B1B7',
+  'danger-main': '#E11428'
 };
+
+export const colorData = [
+  {
+    bg: '#F8FBF9',
+    border: '#B8DBCA',
+    color: '#43936C'
+  },
+  {
+    bg: '#FFFAFA',
+    border: '#F5B1B7',
+    color: '#E11428'
+  },
+  {
+    bg: '#FFFCF5',
+    border: '#FEEABC',
+    color: '#FA9810'
+  },
+  {
+    bg: '#F7FEFF',
+    border: '#41B0B7',
+    color: '#01959F'
+  }
+];
