76507fe8e92c17c49235844a35d12bce53a13e6e
aulyarahman <76601895+aulyarahman@users.noreply.github.com>
2022-08-07 02:43:10 +0800

    add function useTodos for moving todos

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -4,37 +4,40 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTodos } from '../store/slices/todosSlice';
 import { useTodos } from '../services/useTodo.js';
+import { colorData } from '../utils/Colors.js';
 
 const Loading = () => (
   <>
     <Navbar />
     <Containers>
-      <Card>
-        <Skeleton />
-      </Card>
+      <Skeleton />
     </Containers>
   </>
 );
 
-const ItemsCard = ({ items }) => {
+const ItemsCard = ({ items, todoIdx }) => {
   const refCardItems = useRef(null);
-  const { deleteTodos } = useTodos();
+  const { deleteTodos, moveTodos } = useTodos();
   const navigate = useNavigate();
 
   const handleDelete = (idItems, todosId) => {
     return deleteTodos(idItems, todosId);
   };
 
+  const handleMoveTodo = (data, key) => {
+    return moveTodos({ ...data, idx: todoIdx }, key);
+  };
+
   if (!items) return <p>Dont Have Any Items</p>;
 
   return (
     <>
-      {items.map((i, k) => {
+      {items.map((i, key) => {
         const percent = Number(i.progress_percentage);
         return (
           <div
             ref={refCardItems}
-            key={k}
+            key={key}
             draggable={true}
             className={
               'bg-neutral border-[1px] rounded-[4px] p-3 space-y-3 cursor-pointer transition duration-150 hover:transform hover:scale-[1.03] '
@@ -45,6 +48,9 @@ const ItemsCard = ({ items }) => {
               progress={percent}
               href={`/items/add/${i.id}`}
               onClick={(k) => {
+                if (k <= 1) {
+                  handleMoveTodo({ ...i, itemIdx: key }, k);
+                }
                 if (k === 2) {
                   localStorage.setItem('items', JSON.stringify(i));
                   navigate(`/todos/${i.todo_id}/items/${i.id}`);
@@ -61,34 +67,14 @@ const ItemsCard = ({ items }) => {
 
 const Home = () => {
   const navigate = useNavigate();
-  const { data, loading, error } = useSelector((item) => item.todos);
+  const { data, loading, error, status } = useSelector((item) => item.todos);
   const dispatch = useDispatch();
-  const colorData = [
-    {
-      bg: '#F8FBF9',
-      border: '#B8DBCA',
-      color: '#43936C'
-    },
-    {
-      bg: '#FFFAFA',
-      border: '#F5B1B7',
-      color: '#E11428'
-    },
-    {
-      bg: '#FFFCF5',
-      border: '#FEEABC',
-      color: '#FA9810'
-    },
-    {
-      bg: '#F7FEFF',
-      border: '#41B0B7',
-      color: '#01959F'
-    }
-  ];
 
   useEffect(() => {
     const control = new AbortController();
+
     dispatch(fetchTodos());
+
     return () => control.abort();
   }, []);
 
@@ -118,7 +104,7 @@ const Home = () => {
                 {i.title}
               </div>
               <p className="font-bold text-black text-sm">{i.description}</p>
-              <ItemsCard key={i.id} items={i.items} />
+              <ItemsCard key={i.id} items={i.items} todoIdx={i.idx} />
               <div
                 className={'flex gap-2 cursor-pointer'}
                 onClick={() => navigate(`/items/add/${i.id}`)}>
