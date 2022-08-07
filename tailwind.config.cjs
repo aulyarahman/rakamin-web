9617acd3bd228daa9893328d66de4ae96de1efc3
aulyarahman <76601895+aulyarahman@users.noreply.github.com>
2022-08-07 18:57:52 +0800

    add font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,68 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       colors: {
         primary: "#01959F",
+        secondary: "#FEEABC",
+        "danger-border": "#F5B1B7",
+        'danger-main': '#E11428',
         black: "#404040",
         success: "#43936C",
+        'success-border': '#B8DBCA',
         neutral: "#FAFAFA",
+        'neutral-40': '#E0E0E0'
+      },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "move-to-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-200px)",
+          },
+          "100%": {
+            opacity: "70",
+            transform: "translateY(0)",
+          },
+        },
+        "move-to-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(200px)",
+          },
+          "100%": {
+            opacity: "70",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-in-left": {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(300px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+        "move-to-down": "move-to-down 0.5s",
+        "move-to-up": "move-to-up 0.5s",
+        "fade-in-left": "fade-in-left 0.5s ease-out",
       },
     },
     fontWeight: {
@@ -17,9 +72,8 @@ module.exports = {
       sans: ["Nunito Sans"],
     },
     borderRadius: {
-      DEFAULT: "5px",
       sm: "8px",
-      large: "12px",
+      lg: "12px",
       full: "9999px",
     },
     fontSize: {
