375d9238c9847e1cd02963f7a20aa0805f28084f
aulyarahman <76601895+aulyarahman@users.noreply.github.com>
2022-08-07 18:54:01 +0800

    delete layout createTodos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 
-import CreateTodos from '~/layout/CreateTodos';
 import Home from '~/layout/Home';
 import CreateItems from '~/layout/CreateItems.jsx';
 import { ModalConfirmation } from '~/components/ActionSlide/ModalContent';
@@ -9,24 +8,21 @@ import UpdateItems from '~/layout/UpdateItems';
 
 const App = () => {
   const location = useLocation();
-
   return (
     <div>
       <ModalConfirmation />
       {/* */}
       <Routes location={location}>
-        <Route element={<Home />} path="/">
-          <Route element={<CreateTodos />} path="/todos/add" />
-          <Route element={<CreateItems />} path="/items/add/:id" />
-          <Route element={<UpdateItems />} path="/todos/:idTodos/items/:idItems" />
+        <Route element={<Home />} path={`/`}>
+          <Route element={<CreateItems />} path={`/items/add/:id`} />
+          <Route element={<UpdateItems />} path={`/todos/:idTodos/items/:idItems`} />
         </Route>
       </Routes>
 
       {/*  */}
       <Routes>
-        <Route element={<CreateTodos />} path="/todos/add" />
-        <Route element={<CreateItems />} path="/items/add/:id" />
-        <Route element={<UpdateItems />} path="/todos/:idTodos/items/:idItems" />
+        <Route element={<CreateItems />} path={`/items/add/:id`} />
+        <Route element={<UpdateItems />} path={`/todos/:idTodos/items/:idItems`} />
       </Routes>
     </div>
   );
