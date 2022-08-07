84a993df80894a8dbf648ab031b2fb696a9312a9
aulyarahman <76601895+aulyarahman@users.noreply.github.com>
2022-08-07 18:06:50 +0800

    remove suspense

--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import './styles/index.css';
 import ToastContainer from './components/Toast';
+import { APP_VERSION } from '~/utils/app-v';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <ToastContainer />
-      <Router>
-        <React.Suspense fallback={<p>Loading....</p>}>
-          <App />
-        </React.Suspense>
+      <Router basename={APP_VERSION}>
+        <App />
       </Router>
     </Provider>
   </React.StrictMode>
