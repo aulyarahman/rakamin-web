95dd8606af22c519908c63f3cd9c760ec7dab63f
AulyRahman <76601895+aulyarahman@users.noreply.github.com>
2022-09-05 13:27:23 +0800

    Update axios.config.js

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = 'https://todos-project-api.herokuapp.com';
 const TOKEN =
-  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2NjIzNTIyNzB9.sxUpup6EtNvwOUarl6Df_guOUaVJMRFCGjdRTpje8LY';
+  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2NjI3MDEyMjd9.51rGvIzFgXy0YmiZYarySvjnqMo3WJtvGulorsecrt4';
 
 export const Axios = axios.create({
   baseURL: BASE_URL,
